ad6f9f34a0a57e617919e431e6882d56c78d5fa8
Hannes Fostie <hannes.fostie@gmail.com>
2017-08-03 11:37:05 +0200

    add slug and markdown features

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,16 @@
 class Article < ActiveRecord::Base
+  before_validation :set_slug
+
+  def set_slug
+    self.slug = title.strip.parameterize
+  end
+
+  def content=(new_content)
+    write_attribute(:content, new_content)
+    reparse_content
+  end
+
+  def reparse_content
+    self.html_content = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new).render(content)
+  end
 end
